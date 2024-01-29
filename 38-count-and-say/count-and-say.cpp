7b40b4e0470e270395b44dae88e41b413ba5f255
pratham_garg <prathamgarg45@gmail.com>
2024-01-29 22:27:22 +0530

    Time: 28 ms (10.92%) | Memory: 123.8 MB (6.22%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -17,7 +17,7 @@ public:
     }
     string countAndSay(int n) 
     {
-        if(n==1) return "1";
+        // if(n==1) return "1";
         string s = "1";
         for(int i =0 ; i<n-1 ; i++)
         {
