b57a028ff894e02da7e6bc1402ed166b5b62d96c
pratham_garg <prathamgarg45@gmail.com>
2024-01-29 21:58:45 +0530

    Time: 25 ms (11.81%) | Memory: 123.8 MB (6.22%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -3,7 +3,6 @@ public:
     string getString(char x)
 {
     string s(1, x);
- 
     return s;   
 }
     string solve(string s)
@@ -13,22 +12,18 @@ public:
         cout<<s<<endl;
         for(int i=0;i<n;i++)
         {   
-
             if(s[i]==s[i+1])
             {
                 sum++;
             }
-
             else if (s[i]!=s[i+1])
             {
              ans = ans + to_string(sum) +  getString(s[i]); 
              sum=1;
-            }
-            
+            }   
         }
         return ans;
     }
-
     string countAndSay(int n) 
     {
         if(n==1) return "1";
@@ -38,6 +33,5 @@ public:
             s = solve(s);
         }
         return s;
-        
     }
 };
